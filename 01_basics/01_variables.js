c89a74f43b215d22d595ff7a88e03a9eb1bd4d3d
Abhi Aditya <abhiaditya0755@gmail.com>
2025-05-19 08:49:59 +0000

    Let, Var, Const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail = "abhiaditya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountstate;
+
+/* if we declare a variable and do not assign value to them then js take ie a undefined  */
+
+// accountId = 2 :- not allowed 
+accountEmail = "abc@gmail.com"
+accountPassword = "0229"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
+
+
+/*
+we should not use var 
+because of issue in block scope and functional scope.
+
+Block Scope :- Variables declared inside a block (between {}) are only accessible within that block.
+Functional Scope :- Variables declared inside a function are only accessible within that function.
+*/
