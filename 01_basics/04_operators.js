6f14aa6e261bd940ba938dc21c08cdd1036a7dbb
Abhi Aditya <abhiaditya0755@gmail.com>
2025-05-21 19:37:13 +0000

    Operators

---
+++ 01_basics/04_operators.js
@@ -0,0 +1,71 @@
+
+// Operators :- operators are special symbols or keywords used to perform operations on operands (values and variables).
+
+// Types of Operators :-
+
+// Arithmetic Operators :- Used for basic mathematical operations
+/*
++	Addition
+-	Subtraction
+*	Multiplication
+/	Division
+%	Modulus (Remainder)
+**	Exponentiation
+++	Increment
+--	Decrement
+*/
+
+//Assignment Operators :- Used to assign values to variables
+/*
+=	    x = 5	    Assign 5 to x
++=	    x += 5	    x = x + 5
+-=	    x -= 5	    x = x - 5
+*=	    x *= 5	    x = x * 5
+/=	    x /= 5	    x = x / 5
+%=	    x %= 5	    x = x % 5
+**=	    x**= 2	    x = x** 2
+*/
+
+//omparison Operators :- Used to compare values
+/*
+==	    Equal to (loose)
+===	    Equal value and type
+!=	    Not equal (loose)
+!==	    Not equal value or type
+>	    Greater than
+<	    Less than
+>=	    Greater than or equal to
+<=	    Less than or equal to
+*/
+
+// Logical Operators :- Used to combine multiple conditions.
+/*
+&&      AND     T && T = T ; F && F = F ; T && F = F ; F && T = F
+||      OR      T && T = T ; F && F = F ; T && F = T ; F && T = T
+!       NOT     !true = false ; !false = true
+*/
+
+//Ternary Operator :- A shorthand for if-else
+/*
+condition_statement ? If_condition_True : If_condition_False
+*/
+
+// Bitwise Operators :- Operate on binary representations
+/*
+&       AND     1,1=1 ; 1,0=0 ; 0,1=0 ; 0,0=0  
+|       OR      1,1=1 ; 1,0=1 ; 0,1=1 ; 0,0=0
+!       NOT     !1 = false ; !0 = true
+^       XOR     1,1=0 ; 1,0=1 ; 0,1=1 ; 0,0=0
+>>      Right Shift 
+                    {
+                    Each right shift = divide by 2
+                    20 >> 1 = 10
+                    20 >> 2 = 5 
+                    }
+<<      Left Shift 
+                    {
+                    Each Left shift = multiply by 2
+                    5 << 1 = 10
+                    5 << 2 = 20 
+                    }
+*/
